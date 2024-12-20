d712c66887b5c7d04ccc5ce778dc79e5fa4f8ce5
Sven Seeberg <mail@sven-seeberg.de>
2024-12-20 18:34:05 +0100

    Refactor generating answer

--- integreat_chat/chatanswers/views.py
+++ integreat_chat/chatanswers/views.py
@@ -7,15 +7,12 @@ import logging
 
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from django.conf import settings
 
-from integreat_chat.chatanswers.services.answer_service import AnswerService
 from integreat_chat.chatanswers.services.language import LanguageService
 from integreat_chat.chatanswers.services.search import SearchService
-from integreat_chat.chatanswers.services.query_transformer import QueryTransformer
 
 from .services.milvus import UpdateMilvus
-from .utils import translate_source_path
+from .utils import generate_answer
 
 LOGGER = logging.getLogger("django")
 
@@ -99,44 +96,7 @@ def extract_answer(request):
         if "language" not in data or "region" not in data or "message" not in data:
             result = {"status": "error"}
         else:
-            language_service = LanguageService()
-            if data["language"] not in settings.RAG_SUPPORTED_LANGUAGES:
-                rag_language = settings.RAG_FALLBACK_LANGUAGE
-            else:
-                rag_language = data["language"]
-            if (
-                message_language := language_service.classify_language(
-                    data["language"], data["message"]
-                )
-                != data["language"]
-            ):
-                message = language_service.translate_message(
-                    message_language, rag_language, data["message"]
-                )
-            else:
-                message = data["message"]
-
-            answer_service = AnswerService(data["region"], rag_language)
-            if answer_service.needs_answer(data["message"]):
-                if settings.RAG_QUERY_OPTIMIZATION:
-                    qtrans = QueryTransformer(message)
-                    if qtrans.is_transformation_required():
-                        message = qtrans.transform_query()["modified_query"]
-                result = answer_service.extract_answer(message)
-                if rag_language != data["language"]:
-                    result["answer"] = language_service.translate_message(
-                        rag_language, data["language"], result["answer"]
-                    )
-                    old_sources = result["sources"]
-                    result["sources"] = []
-                    for source in old_sources:
-                        result["sources"].append(
-                            translate_source_path(source, data["language"])
-                        )
-                result["status"] = "success"
-                result["message"] = message
-            else:
-                result["status"] = "not a question"
+            result = generate_answer(data)
     return JsonResponse(result)
 
 
