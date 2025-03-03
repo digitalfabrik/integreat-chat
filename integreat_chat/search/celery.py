b993b9dc156c4fa6496355d26262712f49cecd86
Sven Seeberg <mail@sven-seeberg.de>
2025-03-03 22:06:20 +0100

    Fix Celery task import

--- integreat_chat/search/celery.py
+++ integreat_chat/search/celery.py
@@ -5,17 +5,8 @@ Celery worker
 import os
 
 from celery import Celery
-from celery.schedules import crontab
-
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "integreat_chat.core.settings")
 app = Celery("celery_app")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
-
-app.conf.beat_schedule = {
-    'update-indexes': {
-        'task': 'tasks.update_search_indexes',
-        'schedule': crontab(hour=2, minute=30),
-    },
-}
